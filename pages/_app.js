4a6fe9274b3c527cc373429989ef3f178c535558
r-jo <rajbir@rajbir.io>
2021-07-14 07:37:54 -0700

    Moved nav and footer to _app to present on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { Provider } from 'next-auth/client'
-
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
+      <Nav />
       <Component {...pageProps} />
+      <Footer />
     </Provider>
   )
 }
