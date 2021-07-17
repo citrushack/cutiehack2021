850d0feb19b8b6cd7b30471eacd3cdc775ea7a11
Minsoo Kim <minsooerickim@gmail.com>
2021-07-16 23:09:00 -0700

    basic faq
    copied the same structure from the one from citrus hack

---
+++ components/FaqSection.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { FaChevronUp } from "react-icons/fa";
+import styles from "../styles/Accordion.module.css";
+
+export default class FaqSection extends Component {
+    state = {
+      open : false
+    }
+
+    Toggle = () => {
+      this.setState ({
+        open : !this.state.open
+      })
+    }
+
+    render() {
+      return (
+        <div className={(this.state.open ? `${styles.accordionItem} ${styles.open}` : `${styles.accordionItem}`)} onClick={() => this.Toggle()}>
+          <div className={styles.accordionItemHeading}>
+            <div className={styles.accordionItemButton}>
+              <p>{this.props.question}<FaChevronUp className={styles.arrow} /></p>
+            </div>
+          </div>
+          <div className={styles.accordionItemPanel}>
+            <p className={styles.answer}>{this.props.answer}</p>
+          </div>
+        </div>
+      );
+    }
+    
+  };
