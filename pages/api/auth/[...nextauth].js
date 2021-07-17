b8d17ab438d28bc1edd2e96c3225e547746088ba
claynaut <jpesc002@ucr.edu>
2021-07-16 17:11:08 -0700

    Fix issue with signing in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,15 +6,6 @@ const options = {
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: 'read:user user:email',
-      profile(profile) {
-        return {
-          id: profile.id,
-          name: profile.name || profile.login,
-          email: profile.email,
-          image: profile.avatar_url,
-        }
-      },
     }),
 
     Providers.Google({
@@ -27,23 +18,6 @@ const options = {
       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
-    signIn: async (profile, account, metadata) => {
-      // https://developer.github.com/v3/users/emails/#list-email-addresses-for-the-authenticated-user
-      const res = await fetch('https://api.github.com/user/emails', {
-        headers: {
-          Authorization: `token ${account.accessToken}`,
-        },
-      })
-      const emails = await res.json()
-      if (!emails || emails.length === 0) {
-        return
-      }
-      // Sort by primary email - the user may have several emails, but only one of them will be primary
-      const sortedEmails = emails.sort((a, b) => b.primary - a.primary)
-      profile.email = sortedEmails[0].email
-    },
-  },
   pages: {
     signIn: '/signin',
   },
