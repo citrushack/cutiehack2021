875f96410229b210b398021525eb02084a516443
rajbirjohar <github@rajbir.io>
2021-07-20 08:10:13 -0700

    Styled faq

--- components/FaqSection.js
+++ components/FaqSection.js
@@ -1,26 +1,37 @@
-import React, { useState } from 'react';
-import { FaChevronUp } from "react-icons/fa";
-import styles from "../styles/Accordion.module.css";
+import React, { useState } from 'react'
+import { HiPlus } from 'react-icons/hi'
+import styles from '../styles/Accordion.module.css'
+import { motion } from 'framer-motion'
 
 export default function FaqSection(props) {
+  const [open, setOpen] = useState(false)
 
-    const [open, setOpen] = useState(false);
+  const Toggle = () => {
+    setOpen(!open)
+  }
 
-    const Toggle = () => {
-      setOpen(!open);
-    }
-
-    return (
-      <div className={(open ? `${styles.accordionItem} ${styles.open}` : `${styles.accordionItem}`)} onClick={() => Toggle()}>
-        <div className={styles.accordionItemHeading}>
-          <div className={styles.accordionItemButton}>
-            <p>{props.question}<FaChevronUp className={styles.arrow} /></p>
-          </div>
+  return (
+    <div
+      className={
+        open
+          ? `${styles.accordionItem} ${styles.open}`
+          : `${styles.accordionItem}`
+      }
+      onClick={() => Toggle()}
+    >
+      <motion.div
+        whileHover={{ scale: 1.005 }}
+        transition={{ ease: 'easeInOut', duration: 0.1 }}
+        className={styles.accordionItemHeading}
+      >
+        <div className={styles.accordionItemButton}>
+          <h3 className={styles.title}>{props.question}</h3>
+          <HiPlus className={styles.arrow} />
         </div>
-        <div className={styles.accordionItemPanel}>
-          <p className={styles.answer}>{props.answer}</p>
-        </div>
+      </motion.div>
+      <div className={styles.accordionItemPanel}>
+        <p className={styles.answer}>{props.answer}</p>
       </div>
-    );
-    
-  }; 
+    </div>
+  )
+}
