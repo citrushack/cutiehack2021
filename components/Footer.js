2c72e9dc57454354812566502f5132abd81538dd
claynaut <jpesc002@ucr.edu>
2021-07-21 07:01:04 -0700

    Move socials to footer on mobile/smaller screens

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,27 @@
+import React from 'react'
+
+import { IoLogoFacebook, IoLogoInstagram, IoLogoLinkedin } from 'react-icons/io5'
+
 import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Made with ❤️ by the CitrusHack Team.
+      <div className={styles.socialWrapper}>
+        <a href="https://www.facebook.com/cutiehack/">
+          <div><IoLogoFacebook className={styles.icon} /></div>
+        </a>
+        
+        <a href="https://www.instagram.com/cutiehack_ucr/">
+          <div><IoLogoInstagram className={styles.icon} /></div>
+        </a>
+        <a href="https://www.linkedin.com/company/17907222/admin/">
+          <div><IoLogoLinkedin className={styles.icon} /></div>
+        </a>
+      </div>
+      <div className={styles.caption}>
+        Made with ❤️ by the CitrusHack Team.
+      </div>
     </footer>
   )
 }
