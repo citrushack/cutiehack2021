a64b56a069509699a421a474d0af2eec6c32bbca
claynaut <jpesc002@ucr.edu>
2021-08-29 21:53:49 -0700

    Implement modal component, add a pop-up to confirm leaving a group

---
+++ components/Modal.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+
+import { IoCloseCircle } from 'react-icons/io5'
+
+import styles from '../styles/Modal.module.css'
+
+export default function Modal( props ) {
+  return (
+    <>
+      <div className={props.show ? `${styles.modal} ${styles.show}` : `${styles.modal}`}>
+        <div>
+          <div className={styles.header}>
+            <div>
+              <IoCloseCircle className={styles.windowButton} onClick={props.handler}/>
+            </div>
+            <div className={styles.filler}>
+              <IoCloseCircle className={styles.windowButton} />
+            </div>
+          </div>
+          <div className={styles.body}>
+            <h2 className={styles.heading}>{props.header}</h2>
+            <p className={styles.caption}>{props.caption}</p>
+            {props.children}
+          </div>
+        </div>
+      </div>
+      <div onClick={props.handler} className={styles.overlay} />
+    </>
+  )
+}
