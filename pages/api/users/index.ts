271254cd875270e36f68ae84f2394acdf54556be
claynaut <jpesc002@ucr.edu>
2021-09-05 21:37:23 -0700

    Change function name

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { connectToDatabase } from '../../../util/mongodb'
 
-export default async function FetchGroupMembers(req: NextApiRequest, res: NextApiResponse) {
+export default async function FetchUsers(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { db } = await connectToDatabase();
     const users = await db.collection('users').find().toArray();
