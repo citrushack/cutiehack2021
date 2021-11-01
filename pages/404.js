fc9f444c29045ad7853d758cf47e25a4ccbe54da
claynaut <jpesc002@ucr.edu>
2021-11-01 11:53:19 -0700

    Update meta

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,12 @@
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 export default function Custom404() {
   return (
     <Layout>
+      <Head>
+        <title>Cutie Hack | 404</title>
+      </Head>
       <h1>404</h1>
       <h3>Page Not Found</h3>
     </Layout>
--- pages/500.js
+++ pages/500.js
@@ -1,8 +1,12 @@
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 export default function Custom500() {
   return (
     <Layout>
+      <Head>
+        <title>Cutie Hack | 404</title>
+      </Head>
       <h1>500</h1>
       <h3>
         We are having trouble fetching important data right now. Please check
